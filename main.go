56b6f3c83521bcad9b74551b9613dee072a09fbe
Yu Lin <sarah82529@gmail.com>
2022-08-17 22:25:01 +0800

    feat: 調整題目說明及程式碼內容

--- main.go
+++ main.go
@@ -12,31 +12,31 @@ import (
 func main() {
 	// Question 1
 	fmt.Println("=========== Question 1 Start ===========")
-	v1 := q1.Run([]int{0x40a, 0x10b, 0x20c, 0x40d, 0x40e})
+	v1 := q1.Run([]string{"2d", "3d", "4s", "5d", "6d"})
 	fmt.Println(v1)
 	fmt.Println("=========== Question 1 End =============")
 
 	// Question 2
 	fmt.Println("=========== Question 2 Start ===========")
-	v2 := q2.Run([]int{0x308, 0x302, 0x209, 0x104, 0x406})
+	v2 := q2.Run([]string{"2d", "Js", "4d", "5d", "6d"})
 	fmt.Println(v2)
 	fmt.Println("=========== Question 2 End =============")
 
 	// Question 3
 	fmt.Println("=========== Question 3 Start ===========")
-	v3 := q3.Run([]int{0x10c, 0x30c}, []int{0x20c, 0x40b, 0x40c, 0x40d, 0x40e})
+	v3 := q3.Run([]string{"2d", "3d"}, []string{"4d", "5d", "6d", "7d", "8d"})
 	fmt.Println(v3)
 	fmt.Println("=========== Question 3 End =============")
 
 	// Question 4
 	fmt.Println("=========== Question 4 Start ===========")
-	v4 := q4.Run([]int{0x406, 0x407}, []int{0x40a, 0x40b, 0x40c, 0x409, 0x408})
+	v4 := q4.Run([]string{"Ts", "Js"}, []string{"Qs", "Ks", "As", "Td", "Kd"})
 	fmt.Println(v4)
 	fmt.Println("=========== Question 4 End =============")
 
 	// Question 5
 	fmt.Println("=========== Question 5 Start ===========")
-	v5 := q5.Run([]int{0x50f, 0x202}, []int{0x105, 0x309, 0x40a, 0x40b, 0x40c})
+	v5 := q5.Run([]string{"2s", "3c"}, []string{"BJ", "Td", "6c", "7d", "8d"})
 	fmt.Println(v5)
 	fmt.Println("=========== Question 5 End =============")
 }
